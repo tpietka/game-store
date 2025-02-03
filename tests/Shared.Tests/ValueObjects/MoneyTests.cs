3c0bf50b33a1426c88fb179c3e393662974d587f
tpietka <45951259+tpietka@users.noreply.github.com>
2025-02-04 00:26:06 +0100

    Add first Money test

--- tests/Shared.Tests/ValueObjects/MoneyTests.cs
+++ tests/Shared.Tests/ValueObjects/MoneyTests.cs
@@ -1,10 +1,14 @@
+using Shared.ValueObjects;
+
 namespace Shared.Tests.ValueObjects;
 
 public class MoneyTests
 {
     [Fact]
-    public void Test1()
+    public void MoneyToString_ReturnsFormattedText()
     {
+        var money = new Money(100, Currency.Default);
 
+        Assert.Equal("100 PLN", money.ToString());
     }
 }
