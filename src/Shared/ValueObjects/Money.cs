ad19f5b48790226c308a6d35087ac39c237c401a
tpietka <45951259+tpietka@users.noreply.github.com>
2025-02-04 00:26:51 +0100

    Add Money constructor and ToString method

--- src/Shared/ValueObjects/Money.cs
+++ src/Shared/ValueObjects/Money.cs
@@ -1,8 +1,25 @@
 ﻿namespace Shared.ValueObjects;
 
-public record Money(decimal Value, Currency Currency)
+public record Money
 {
-    public static implicit operator decimal(Money money) => money.Value;
-    public static explicit operator Money(decimal value) => new Money(value, Currency.Default);
+    public decimal Amount { get; }
+    public Currency Currency { get; }
+
+    public Money(decimal amount, Currency currency)
+    {
+        if (currency is null)
+        {
+            throw new ArgumentException("Currency cannot be empty.", nameof(currency));
+        }
+
+        Amount = amount;
+        Currency = currency;
+    }
+
+    public override string ToString() => $"{Amount} {Currency.Value}";
+
+
+    public static implicit operator decimal(Money money) => money.Amount;
+    public static explicit operator Money(decimal amount) => new Money(amount, Currency.Default);
 
 }
